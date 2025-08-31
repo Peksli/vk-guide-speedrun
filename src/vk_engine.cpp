09be24d2d18c7375ae39dbfa2067fdfd74f64440
Ilya <ilyayakub67@gmail.com>
2025-08-31 16:34:36 +0300

    Init commit

---
+++ src/vk_engine.cpp
@@ -0,0 +1,152 @@
+﻿//> includes
+#include "vk_engine.h"
+
+#include <SDL.h>
+#include <SDL_vulkan.h>
+
+#include <vk_initializers.h>
+#include <vk_types.h>
+
+#include "VkBootstrap.h"
+
+#include <chrono>
+#include <thread>
+
+constexpr bool bUseValidationLayers = false;
+
+VulkanEngine* loadedEngine = nullptr;
+
+VulkanEngine& VulkanEngine::Get() { return *loadedEngine; }
+void VulkanEngine::init()
+{
+    // only one engine initialization is allowed with the application.
+    assert(loadedEngine == nullptr);
+    loadedEngine = this;
+
+    // We initialize SDL and create a window with it.
+    SDL_Init(SDL_INIT_VIDEO); // SDL_INIT_VIDEO - main windowing functionality(like events)
+
+    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_VULKAN);
+
+    _window = SDL_CreateWindow(
+        "Vulkan Engine",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        _windowExtent.width,
+        _windowExtent.height,
+        window_flags);
+
+	init_vulkan();
+
+	init_swapchain();
+
+	init_commands();
+
+	init_sync_structures();
+
+    // everything went fine
+    _isInitialized = true;
+}
+
+
+void VulkanEngine::init_vulkan()
+{
+    vkb::InstanceBuilder builder;
+    auto inst_ret = builder.set_app_name("Vulkan app")
+        .request_validation_layers(bUseValidationLayers)
+        .use_default_debug_messenger()
+        .require_api_version(1, 3, 0)
+        .build();
+
+    vkb::Instance vkb_instance = inst_ret.value();
+    _instance = vkb_instance.instance;
+    _debug_messenger = vkb_instance.debug_messenger;
+
+    SDL_Vulkan_CreateSurface(_window, _instance, _surface);
+
+    // Vk 1.3
+    VkPhysicalDeviceVulkan13Features features{ .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES };
+    features.dynamicRendering = true;
+    features.synchronization2 = true;
+
+    VkPhysicalDeviceVulkan12Features features12{ .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES };
+    features12.bufferDeviceAddress = true; // will let us use GPU pointers without binding buffers
+    features12.descriptorIndexing = true; // gives us bindless textures
+
+    vkb::PhysicalDeviceSelector selector{ vkb_instance };
+    vkb::PhysicalDevice physcialDevice = selector
+        .set_minimum_version(1, 3)
+        .set_required_features_13(features)
+        .set_required_features_12(features12)
+        .set_surface(_surface)
+        .select()
+        .value();
+
+    vkb::DeviceBuilder deviceBuilder{ physicalDevice };
+    vkb::Device vkbDevice = deviceBuilder.build().value();
+.
+}
+void VulkanEngine::init_swapchain()
+{
+	//nothing yet
+}
+void VulkanEngine::init_commands()
+{
+	//nothing yet
+}
+void VulkanEngine::init_sync_structures()
+{
+	//nothing yet
+}
+
+
+
+void VulkanEngine::cleanup()
+{
+    if (_isInitialized) {
+
+        SDL_DestroyWindow(_window);
+    }
+
+    // clear engine pointer
+    loadedEngine = nullptr;
+}
+
+void VulkanEngine::draw()
+{
+    // nothing yet
+}
+
+void VulkanEngine::run()
+{
+    SDL_Event e;
+    bool bQuit = false;
+
+    // main loop
+    while (!bQuit) {
+        // Handle events on queue
+        while (SDL_PollEvent(&e) != 0) { // This will ask SDL for all of the events the OS has sent to the application
+            // close the window when user alt-f4s or clicks the X button
+            if (e.type == SDL_QUIT)
+                bQuit = true;
+
+            if (e.type == SDL_WINDOWEVENT) {
+                if (e.window.event == SDL_WINDOWEVENT_MINIMIZED) {
+                    stop_rendering = true;
+                }
+                if (e.window.event == SDL_WINDOWEVENT_RESTORED) {
+                    stop_rendering = false;
+                }
+            }
+        }
+
+        // do not draw if we are minimized
+        if (stop_rendering) {
+            // throttle the speed to avoid the endless spinning
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            continue;
+        }
+
+        draw();
+    }
+}
